f866028d79c6625b0570d8c7928d8b089eedd0f5
Griffin-Dalby <griffindalby7@outlook.com>
2025-08-14 21:06:50 -0600

    [util.states] Created inital module

--- src/ReplicatedStorage/Sawdust/__impl/util/states/init.lua
+++ src/ReplicatedStorage/Sawdust/__impl/util/states/init.lua
@@ -1,1 +1,18 @@
-return {}
+--[[
+
+    Sawdust State Machine
+
+    Griffin Dalby
+    2025.08.13
+
+    This implementation will make it easy to set up and manage a state machine.
+
+--]]
+
+--]] Services
+--]] Modules
+--]] Settings
+--]] Constants
+--]] Variables
+--]] Functions
+--]] Implementation
