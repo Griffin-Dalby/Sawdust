4cd49fe2d345d2bd5fd78825aec24d8972847eb8
Griffin-Dalby <griffindalby7@outlook.com>
2025-09-04 22:28:32 -0600

    [util.state] Sorting priority list

--- src/ReplicatedStorage/Sawdust/__impl/util/states/state/init.lua
+++ src/ReplicatedStorage/Sawdust/__impl/util/states/state/init.lua
@@ -84,13 +84,15 @@ function state:entered() : boolean
     if self.__update then
         self.__update:Disconnect() end
 
-    local ordered_transitions = {}
+    local prioritized_list = {}
     for _, transition : __type.StateTransition in pairs(self.transitions) do
-        if ordered_transitions[transition.__priority] then
+        if prioritized_list[transition.__priority] then
             error(`there are multiple transitions @ priority {tostring(transition.__priority)}!`)
             break end
-        ordered_transitions[transition.__priority] = transition
+        prioritized_list[transition.__priority] = transition
     end
+    table.sort(prioritized_list, function(a, b)
+        return a.__priority > b.priority end)
 
     self.__update = runService.Heartbeat:Connect(function(delta)
         self.environment.total_state_time+=delta
@@ -103,7 +105,7 @@ function state:entered() : boolean
 
         --] Run Transition Conditions
         local did_transition = false
-        for priority: number, i_transition: __type.StateTransition in pairs(ordered_transitions) do
+        for priority: number, i_transition: __type.StateTransition in pairs(prioritized_list) do
             did_transition = i_transition:runConditionals()
             if did_transition then break end
         end
