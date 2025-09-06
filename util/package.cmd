6100323a47110b081849063659164e3b3653ab99
Griffin-Dalby <griffindalby7@outlook.com>
2025-09-05 18:49:28 -0600

    [Sawdust] Updated package.cmd to build w/ rojo

--- util/package.cmd
+++ util/package.cmd
@@ -1,3 +1,8 @@
 @echo off
 set /p version=Package version: 
-wally package --output build/sawdust-v%version%
+echo Package for Wally...
+wally package -v --output build/sawdust-v%version%
+
+echo Build for Release...
+rojo build -v --output build/sawdust-v%version%.rbxl
+rojo build -v --output build/sawdust-v%version%.rbxmx
