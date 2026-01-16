87eed38e69612c689e301c61f32f0594ac6aea01
Griffin-Dalby <griffindalby7@outlook.com>
2026-01-16 00:28:20 -0700

    document(debounce): Cleaned up Debounce utility to adhere to new typesolver

--- src/__impl/util/debounce/init.lua
+++ src/__impl/util/debounce/init.lua
@@ -1,3 +1,4 @@
+--!strict
 --[[
 
     Debounce Utility
@@ -18,74 +19,115 @@ local cache = require(__impl.cache)
 local debounceCache = cache.findCache('__debounce_cache')
 
 --]] Debounce
-local debounce = {}
-debounce.__index = debounce
-debounce.shared  = nil :: DebounceTracker
+type self_methods = {
+    __index: self_methods,
+    shared: DebounceTracker,
+
+    --[[ CONSTRUCTOR ]]--
+
+    --[[
+        This will construct a new DebounceTracker, where you can :Track(), 
+        :Cancel(), and :Check() a debounce entry.
+        
+        @return DebounceTracker
+    ]]
+    NewTracker: () -> DebounceTracker,
+
+    --[[ METHODS ]]--
+
+    --[[
+        This will add new debounce entry to the tracked list with a provided lifetime.
+
+        After the lifetime, the cleanup callback will be called, providing a
+        a "exists" paramater, true if the tracked debounce existed past the total lifetime.
+        
+        @param debounce_id ID of the debounce to track
+        @param lifetime Time until debounce ends
+        @param cleanup Optional cleanup callback function
+    ]]
+    Track: (self: DebounceTracker, debounce_id: string, lifetime: number, cleanup: ((exists: boolean) -> nil)?) -> nil,
+
+    --[[
+        Manually cancel a specific debounce.
+        
+        @param debounce_id Debounce to cancel
+    ]]
+    Cancel: (self: DebounceTracker, debounce_id: string) -> nil,
+    
+    --[[
+        Checks for the presence of debounce with a specific ID in the
+        tracked debounce list is active.
+        
+        @param debounce_id Debounce ID to check
+
+        @return boolean Debounce Active
+    ]]
+    Check: (self: DebounceTracker, debounce_id: string) -> nil,
+
+    --[[
+        Properly cleans up this object.
+    ]]
+    Discard: (self: DebounceTracker) -> nil
+}
+
+local Debounce = {} :: self_methods
+Debounce.__index = Debounce
 
 type self = {
     __tracked: {[string]: boolean}
 }
-export type DebounceTracker = typeof(setmetatable({} :: self, debounce))
+export type DebounceTracker = typeof(setmetatable({} :: self, {} :: self_methods))
 
---]] Debounce Functions
-
---[[ debounce.newTracker(): DebounceTracker
-    This will construct a new DebounceTracker, where you can :track(), 
-    :cancel(), and :check(). ]]
-function debounce.newTracker() : DebounceTracker
-    local self = setmetatable({} :: self, debounce)
+--[[ CONSTRUCTOR ]]--
+function Debounce.NewTracker() : DebounceTracker
+    local self = setmetatable({} :: self, Debounce)
 
     self.__tracked = {}
 
     return self
 end
 
---]] Setup Shared
+--> Setup Shared
 if not debounceCache:hasEntry('shared') then
-    local sharedTracker = debounce.newTracker()
-    debounce.shared = sharedTracker end
+    local sharedTracker = Debounce.NewTracker()
+    Debounce.shared = sharedTracker end
 
---[[ debounce:track(debounceId: string, lifetime: number, cleanup: (exists: boolean) -> nil)
-    This will add $debounceId to the tracked list for the $lifetime.
-
-    After the lifetime, $cleanup will be called. The "exists" argument
-    dictates if the tracked debounce existed after the total lifetime. ]]
-function debounce:track(debounceId: string, lifetime: number, cleanup: (exists: boolean) -> nil)
+--[[ METHODS ]]--]]
+function Debounce:Track(debounce_id: string, lifetime: number, cleanup: ((exists: boolean) -> nil)?)
     assert(self, `Attempt to :track() without constructing.`)
-    assert(not self.__tracked[debounceId], `Attempt to track "{debounceId}" while it's already being tracked!`) --// TODO: Review & refactor error severity
+    assert(not self.__tracked[debounce_id], `Attempt to track "{debounce_id}" while it's already being tracked!`) --// TODO: Review & refactor error severity
 
-    self.__tracked[debounceId] = true
+    self.__tracked[debounce_id] = true
     task.delay(lifetime, function()
-        if not self.__tracked[debounceId] then cleanup(false); return end
+        if not self.__tracked[debounce_id] and cleanup then cleanup(false); return end
 
-        self.__tracked[debounceId] = nil
+        self.__tracked[debounce_id] = nil
         if cleanup then cleanup(true) end
     end)
+
+    return nil
 end
 
---[[ debounce:cancel(debounceId: string)
-    Manually cancel a specific debounce. ]]
-function debounce:cancel(debounceId: string)
+function Debounce:Cancel(debounce_id: string)
     assert(self, `Attempt to :cancel() without constructing.`)
-    self.__tracked[debounceId] = nil
+    self.__tracked[debounce_id] = nil
+
+    return nil
 end
 
---[[ debounce:check(debounceId: string) : boolean
-    Checks for the presence of $debounceId in the tracked debounce
-    list, if it exists, true will be returned. ]]
-function debounce:check(debounceId: string) : boolean
+function Debounce:Check(debounce_id: string) : boolean
     assert(self, `Attempt to :check() without constructing.`)
 
-    return (self.__tracked[debounceId]==true)
+    return (self.__tracked[debounce_id]==true)
 end
 
---[[ debounce:discard()
-    Properly cleans up all debounces. ]]
-function debounce:discard()
+function Debounce:Discard()
     for debounceId: string in pairs(self.__tracked) do
-        self:cancel(debounceId) end
+        self:Cancel(debounceId) end
 
     table.clear(self)
+
+    return nil
 end
 
-return debounce
+return Debounce
