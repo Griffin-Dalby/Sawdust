2d93947b259129293327fa4c70b7e0c73373389a
Griffin-Dalby <griffindalby7@outlook.com>
2026-01-16 00:27:16 -0700

    fix(sawdust): Fixed dev-util gitignore overlapping with util modules

--- util/package.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set /p version=Package version: 
-echo Package for Wally...
-wally package -v -v -v --output build/sawdust-v%version%
-
-echo Build for Release...
-rojo build -v -v -v --output build/sawdust-v%version%.rbxl
